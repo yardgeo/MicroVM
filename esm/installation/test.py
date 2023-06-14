99d02b67319e6c690dcbbc729b8e6d5122e5a282
Georgii Iardukhin <43379766+yardgeo@users.noreply.github.com>
2023-06-14 20:59:44 +0300

    Create test.py

---
+++ esm/installation/test.py
@@ -0,0 +1,23 @@
+mport torch
+import esm
+
+model = esm.pretrained.esmfold_structure_module_only_8M()
+model = model.eval().cuda()
+
+# Optionally, uncomment to set a chunk size for axial attention. This can help reduce memory.
+# Lower sizes will have lower memory requirements at the cost of increased speed.
+# model.set_chunk_size(128)
+
+sequence = "MKTVRQERLKSIVRILERSKEPVSGAQLAEELSVSRQVIVQDIAYLRSLGYNIVATPRGYVLAGG"
+# Multimer prediction can be done with chains separated by ':'
+
+with torch.no_grad():
+    output = model.infer_pdb(sequence)
+
+with open("result.pdb", "w") as f:
+    f.write(output)
+
+import biotite.structure.io as bsio
+struct = bsio.load_structure("result.pdb", extra_fields=["b_factor"])
+print(struct.b_factor.mean())  # this will be the pLDDT
+# 88.3
